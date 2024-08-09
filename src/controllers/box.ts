4572281106377ac4d0a5c95bc1801ef6c78bbd5f
unknown <mikhaylov.vlaadislav@yandex.ru>
2024-08-10 01:03:52 +0300

    fix bag

--- src/controllers/box.ts
+++ src/controllers/box.ts
@@ -3,12 +3,13 @@ import { createBoxService } from './../service/box-service';
 import { Response, Request } from "express";
 
 
-export const createBox = (req: Request, res: Response) => {
-    try {
-        const data = createBoxService(req.body, req.file as Express.Multer.File, req.user.account.id)
-    } catch (error) {
-
-    }
+export const createBox = async (req: Request, res: Response) => {
+	try {
+		const box = await createBoxService(req.body, req.file as Express.Multer.File, req.user.account.id)
+		return res.status(200).json(box)
+	} catch (error) {
+		console.log(error)
+	}
 }
 
 export const getBoxes = () => { }
--- src/service/box-service.ts
+++ src/service/box-service.ts
@@ -6,86 +6,89 @@ import boxModel from '../models/box-model';
 
 
 export interface IData {
-    useWish: boolean
-    cashLimitMax: number
-    cashLimitCurrency: string
-    cashLimitMin: number
-    nameBox: string,
-    picture: string | null,
-    useCashLimit: boolean,
-    useNames: boolean,
-    usePhone: boolean,
-    useEmail: boolean
+	useWish: boolean
+	cashLimitMax: number | null
+	cashLimitCurrency: string
+	cashLimitMin: number | null
+	nameBox: string,
+	picture: string | null,
+	useCashLimit: boolean,
+	useNames: boolean,
+	usePhone: boolean,
 
-    [key: string]: string | number | boolean | null | undefined;
+	[key: string]: string | number | boolean | null | undefined;
 }
 
 
 export const createBoxService = async (data: IData, file: Express.Multer.File, userId: string) => {
-    try {
-        const boxRules = transformObject(data)
-        const user = await userModel.findById(userId)
-        if (!user) return { error: { message: 'User not found' } }
+	try {
+		const boxRules = transformObject(data)
+		const user = await userModel.findById(userId)
+		if (!user) return { error: { message: 'User not found' } }
 
-        const inviteLink = uuidv4()
+		const box = new boxModel({
+			box: {
+				useWish: boxRules.useWish,
+				cashLimitMax: boxRules.cashLimitMax,
+				cashLimitCurrency: boxRules.cashLimitCurrency,
+				cashLimitMin: boxRules.cashLimitMin,
+				nameBox: boxRules.nameBox,
+				useCashLimit: boxRules.useCashLimit,
+				useNames: boxRules.useNames,
+				usePhone: boxRules.usePhone,
+			},
+			admin: {
+				email: user.email,
+				username: user.username,
+			},
+		})
 
+		if (file) {
+			box.box.logo = file.filename
+			box.box.picture = null
+		} else if (boxRules.picture) {
+			box.box.picture = boxRules.picture
+			box.box.logo = null
+		}
 
-        const box = new boxModel({
-            box: {
-                useWish: boxRules.useWish,
-                cashLimitMax: boxRules.cashLimitMax,
-                cashLimitCurrency: boxRules.cashLimitCurrency,
-                cashLimitMin: boxRules.cashLimitMin,
-                inviteLink: inviteLink,
-                // logo: { type: String },
-                nameBox: boxRules.nameBox,
-                // picture: { type: String },
-                useCashLimit: boxRules.useCashLimit,
-                useNames: boxRules.useNames,
-                usePhone: boxRules.usePhone,
-                useEmail: boxRules.useEmail
-            },
-            admin: {
-                email: user.email,
-                username: user.username,
-            },
-        })
-        user.boxes.push(box._id)
-        await box.save()
-        await user.save()
-        console.log(box)
-    } catch (error) {
-        console.log(error)
-    }
+		user.boxes.push(box._id)
+		box.box.inviteLink = `http://localhost:5173/box?join=${String(box._id)}`
+
+		await box.save()
+		await user.save()
+		return box._id
+	} catch (error) {
+		console.log(error)
+	}
 
 
 }
 
 const transformObject = (obj: IData) => {
-    for (let key in obj) {
-        if (obj[key] === "null") {
-            obj[key] = null;
-        } else if (obj[key] === "false") {
-            obj[key] = false;
-        } else if (obj[key] === "true") {
-            obj[key] = true;
-        } else if (obj[key] === "undefined") {
-            obj[key] = undefined;
-        } else if (typeof obj[key] === 'string' && !isNaN(Number(obj[key]))) {
-            obj[key] = Number(obj[key]);
-        }
-    }
-    return obj;
+	for (let key in obj) {
+		if (obj[key] === "null") {
+			obj[key] = null;
+		} else if (obj[key] === "false") {
+			obj[key] = false;
+		} else if (obj[key] === "true") {
+			obj[key] = true;
+		} else if (obj[key] === "undefined") {
+			obj[key] = undefined;
+		} else if (typeof obj[key] === 'string' && !isNaN(Number(obj[key]))) {
+			obj[key] = Number(obj[key]);
+		}
+	}
+	return obj;
 }
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'src/uploads/');
-    },
-    filename: (req, file, cb) => {
-        const uniqueName = `${uuidv4()}-${Date.now()}${path.extname(file.originalname)}`;
-        cb(null, uniqueName);
-    }
+	destination: (req, file, cb) => {
+		cb(null, 'src/uploads/');
+	},
+	filename: (req, file, cb) => {
+		const uniqueName = `${uuidv4()}-${Date.now()}${path.extname(file.originalname)}`;
+		cb(null, uniqueName);
+	}
 });
 export const upload = multer({ storage: storage })
